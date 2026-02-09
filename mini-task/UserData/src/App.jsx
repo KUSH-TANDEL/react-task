69f8e24974d51b28672b27a11bf1fac4b37d0ed2
KUSH-TANDEL <kushtandel987@gmail.com>
2026-02-09 12:32:25 +0530

    Implement pagination for user data display

--- mini-task/UserData/src/App.jsx
+++ mini-task/UserData/src/App.jsx
@@ -6,32 +6,45 @@ import { userData } from './UserData'
 
 function App() {
   const [users, setUsers] = useState([])
-  const [count,setCount] = useState(0)
+  const [page, setPage] = useState(1)
+  const [limit, setLimit] = useState(8)
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await userData();
-      setUsers(data)
+      setUsers(data.slice((page - 1) * limit, page * limit));
+      
     }
     fetchData();
-  }, [])
+  }, [page, limit]);
 
   console.log(users);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(userData().length / limit); i++) {
+    pageNumbers.push(i);
+  }
+
+const totalpages = Math.ceil( userData().length / limit);
+
+
 
 
   return (
     <>
-  <div className='cards'>
+  <div className='cards w-100'>
       {
         users.map((user) => (
+          <div key={user.id} className=''>
             <div key={user.id}  className="card my-4 mx-4 ">
               <img src={user.image} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">{user.firstName}</h5>
               </div>
               <div className="card-footer">
-                <small className="text-muted">{user.address.address}</small>
+                <small className="text-muted flex-grow-1 ">{user.address.address}</small>
               </div>
+            </div>
             </div>
         ))
       }    
@@ -39,47 +52,26 @@ function App() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <div>
-  <a href="https://vite.dev" target="_blank">
-    <img src={viteLogo} className="logo" alt="Vite logo" />
-  </a>
-  <a href="https://react.dev" target="_blank">
-    <img src={reactLogo} className="logo react" alt="React logo" />
-  </a>
+<div className="d-flex justify-content-center my-4">
+<nav aria-label="Page navigation example">
+  <ul className="pagination">
+    <li className="page-item"><button className="page-link" onClick={() => setPage(page>1?page - 1: page )}>Previous</button></li>
+    {page > 1 && (
+      <li className="page-item"><button className="page-link" onClick={() => setPage(page - 1)}>{page - 1}</button></li>
+    )}
+    <li className="page-item active"><button className="page-link" onClick={() => setPage(page)}>{page}</button></li>
+    <li className="page-item"><button className="page-link" onClick={() => setPage(page + 1 )}>{page + 1}</button></li>
+    <li className="page-item"><button className="page-link" onClick={() => setPage(page + 2)}>{page + 2}</button></li>
+    <li className="page-item"><button className="page-link" onClick={() => setPage(page + 1 )}>Next</button></li>
+  </ul>
+</nav>
 </div>
-<h1>Vite + React</h1>
-<div className="card">
-  <div>
-  <button onClick={() => setCount((count) => count + 1)}>
-    Add {count}
-  </button>
-  <button onClick={() => setCount((count) => count>0?count - 1:0)}>
-    Subtract {count}
-  </button>
-  </div>
-  <p>
-    Edit <code>src/App.jsx</code> and save to test HMR
-  </p>
-</div>
-<p className="read-the-docs">
-  Click on the Vite and React logos to learn more
-</p> */}
-
     </>
+
+
+
+
+
   )
 }
 
